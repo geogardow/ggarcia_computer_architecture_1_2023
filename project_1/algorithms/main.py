e29a51c594f6f3aa65fe9236d3c38e3d4bfd6921
JustinFB18 <jusfb18@gmail.com>
2023-09-24 13:39:25 -0600

    Add link snf vompile with Python Automatic

---
+++ project_1/algorithms/main.py
@@ -0,0 +1,28 @@
+import subprocess
+import time
+
+file = "mul_test"
+
+comp = ["arm-none-eabi-as", file+".s", "-g", "-o", file+".o"]
+link = ["arm-none-eabi-ld", file+".o", "-o", file]
+
+subprocess.Popen(comp).wait()
+subprocess.Popen(link).wait()
+
+# Ejecutar de qemu en el puerto 8080
+qemu_command = ["qemu-arm", "-singlestep", "-g", "8080", file]
+
+# Ejecutar GDB y conectarlo al puerto 8080
+gdb_commands = ["arm-none-eabi-gdb", "-tui",
+                file, "-ex", "target remote localhost:8080"]
+
+# Ejecuta QEMU en un proceso separado
+qemu_process = subprocess.Popen(qemu_command)
+time.sleep(1)
+
+# Ejecuta GDB en un proceso separado y lo conecta a QEMU
+gdb_process = subprocess.Popen(gdb_commands)
+
+# Espera a que ambos procesos finalicen
+qemu_process.wait()
+gdb_process.wait()
