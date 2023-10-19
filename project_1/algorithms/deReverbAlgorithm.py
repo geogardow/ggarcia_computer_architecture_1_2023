d9f6cf5bf438e2e7e8de4697da3931c7d8c92875
Diana Mejías Hernández <85594834+dmejias08@users.noreply.github.com>
2023-10-19 17:30:34 -0600

    scripts de alto nivel de algoritmos

--- project_1/algorithms/deReverbAlgorithm.py
+++ project_1/algorithms/deReverbAlgorithm.py
@@ -1,4 +1,23 @@
 from audio_preprocessing import *
+import array
+import wave
+
+def create_audio(audio_data, path):
+
+    sample_width = 2  # 8 bytes (64-bit)
+    num_channels = 1  # Mono audio
+    sample_rate = 32000 
+
+    with wave.open(path, "w") as wf:
+        wf.setnchannels(num_channels)
+        wf.setsampwidth(sample_width)
+        wf.setframerate(sample_rate)
+
+        # Convert and write the audio data to the file
+        audio_array = array.array('h', [int(x * 2047) for x in audio_data])  # Scale to 16-bit range
+        #print(audio_array)
+        wf.writeframes(audio_array.tobytes())
+    print("WAV file created successfully.")
 
 def getDeReverb(data, k, a):
     result = []
@@ -9,6 +28,8 @@ def getDeReverb(data, k, a):
             result.append((1/(1-a))*data[i])
     return result
 
-file_path = "./project_1/algorithms/audioConReverb.wav"
-deReverbSamples = getDeReverb(read_wav_file(file_path, 32000, 5),1600, 0.6)
-print(deReverbSamples)
+file_path_to_open = "./project_1/algorithms/audioConReverb.wav"
+file_path_to_create = "./project_1/algorithms/substractedReverb.wav"
+deReverbSamples = getDeReverb(read_wav_file(file_path_to_open, 32000, 4),1600, 0.6)
+
+create_audio(deReverbSamples, file_path_to_create)
--- project_1/algorithms/reverbAlgorithm.py
+++ project_1/algorithms/reverbAlgorithm.py
@@ -1,4 +1,23 @@
 from audio_preprocessing import *
+import array
+import wave
+
+def create_audio(audio_data, path):
+
+    sample_width = 2  # 8 bytes (64-bit)
+    num_channels = 1  # Mono audio
+    sample_rate = 32000 
+
+    with wave.open(path, "w") as wf:
+        wf.setnchannels(num_channels)
+        wf.setsampwidth(sample_width)
+        wf.setframerate(sample_rate)
+
+        # Convert and write the audio data to the file
+        audio_array = array.array('h', [int(x * 32767) for x in audio_data])  # Scale to 16-bit range
+        #print(audio_array)
+        wf.writeframes(audio_array.tobytes())
+    print("WAV file created successfully.")
 
 def getReverb(data, k, a):
     result = []
@@ -8,6 +27,8 @@ def getReverb(data, k, a):
         else:
             result.append((1-a)*data[i])
     return result
-file_path = "./project_1/algorithms/audioSinReverb.wav"
-reverbSamples = getReverb(read_wav_file(file_path, 32000, 5),1600, 0.6)
-print(reverbSamples)
+
+file_path_to_open = "./project_1/algorithms/audioSinReverb.wav"
+file_path_to_create = "./project_1/algorithms/addedReverb.wav"
+reverbSamples = getReverb(read_wav_file(file_path_to_open, 32000, 4),1600, 0.6)
+create_audio(reverbSamples, file_path_to_create)
